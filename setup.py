15e7e455e3389ca266e4ac97f04532ed3dec986f
William <liuwilliam.zh@gmail.com>
2021-02-11 16:21:40 +0800

    build(setup.py): 修改 setup.py, 更新版本号为 0.2.5.
    修改 setup.py, 添加依赖 pymediainfo 和 ffmpeg-python, 更新版本号为 0.2.5.

--- setup.py
+++ setup.py
@@ -2,25 +2,29 @@
 
 from setuptools import setup, find_packages
 
-VERSION = '0.2.4'
+VERSION = '0.2.5'
 
 setup(name='GetThumbnails',
       version=VERSION,
       description="A FFmpeg based thumbnails tool written by Python.",
       long_description='Find more in https://github.com/liuwilliamBUPT/GetThumb',
-      classifiers=[],  # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[],
+      # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       keywords='python FFmpeg thumbnails',
       author='William Liu',
       author_email='liuwilliam.zh@gmail.com',
       url='https://github.com/liuwilliamBUPT/GetThumb',
       license='GPL v3.0',
       packages=find_packages(),
+      install_requires=[
+          'pymediainfo',
+          'ffmpeg-python',
+      ],
       include_package_data=True,
       zip_safe=True,
-      install_requires=[],
       entry_points={
-        'console_scripts': [
-            'GetThumbnails = thumbnails.main:main'
-        ]
+          'console_scripts': [
+              'GetThumbnails = thumbnails.main:main'
+          ]
       },
-)
+      )
